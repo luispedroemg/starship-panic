4959e5d5c80ab310d84d4220bb5726d6a164c210
The Dev <luispedro.emg@gmail.com>
2018-10-10 17:05:34 +0100

    haha

--- SpaceParticles.gd
+++ SpaceParticles.gd
@@ -18,9 +18,15 @@ func _generate_particle():
 	var chosen_lane_idx = randi() % self.lanes.size()
 	var z = self.lanes[chosen_lane_idx].global_transform.origin.z
 	var space_particle = SpaceParticle.instance()
+	space_particle.get_node("MeshInstance/CollisionArea").set_collision_layer_bit(1, false);
+	space_particle.get_node("MeshInstance/CollisionArea").set_collision_mask_bit(0, false);
+	space_particle.get_node("MeshInstance/CollisionArea").set_collision_mask_bit(2, false);
 	self.add_child(space_particle)
 	space_particle.global_transform.origin = Vector3(1000 - randf()*700, 2, z)
 	space_particle.get_child(0).connect("dead_particle", self, "on_dead_particle")
+	space_particle.get_node("MeshInstance/CollisionArea").set_collision_layer_bit(1, true);
+	space_particle.get_node("MeshInstance/CollisionArea").set_collision_mask_bit(0, true);
+	space_particle.get_node("MeshInstance/CollisionArea").set_collision_mask_bit(2, true);
 
 func start(lanes):
 	self.lanes = lanes
